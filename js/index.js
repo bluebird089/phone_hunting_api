b5d7ee0cbbc4d2489d95eac2ea9eaab64b06a122
bluebird089 <bluebird089@proton.me>
2023-09-01 12:19:40 +0600

    added full project

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText = 'i', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
@@ -9,6 +9,14 @@ const displayPhones = (phones, isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.innerHTML = ``;
     const showAllButton = document.getElementById('show-all-button');
+    if (phones.length <= 0){
+        const phoneCard = document.createElement('div');
+        phoneCard.innerHTML = `
+        <h4 class="text-xl"><i>No Phone Available</i><h4>
+        `;
+        phoneContainer.classList = `text-center`;
+        phoneContainer.appendChild(phoneCard);
+    }
     if (phones.length > 12 && !isShowAll) {
         showAllButton.classList.remove('hidden');
     }
@@ -26,7 +34,7 @@ const displayPhones = (phones, isShowAll) => {
         <div class="text-center">
             <h2 class="text-lg md:text-2xl font-semibold">${phone.phone_name}</h2>
             <div class="card-actions justify-center">
-                <button class="btn bg-slate-600 text-white ml-3 hover:bg-slate-500 mt-5">Buy Now</button>
+                <button onclick="handleDetails('${phone.slug}');" class="btn bg-slate-600 text-white ml-3 hover:bg-slate-500 mt-5">Details</button>
             </div>
         </div>
         `
@@ -55,3 +63,26 @@ const toggleLoading = (isLoading) => {
 const handleShowAll = () => {
     handleSearch(true);
 }
+
+const handleDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    const showDetailsContainer = document.getElementById('show-details-container');
+    showDetailsContainer.innerHTML = `
+    <img class="mx-auto" src="${phone.image}" alt="phone" />
+    <h3 class="text-center font-semibold text-2xl py-2">${phone.name}</h3>
+    <p><span class="font-semibold">Storage:</span> ${phone.mainFeatures.storage}</p>
+    <p><span class="font-semibold">Display Size:</span> ${phone.mainFeatures.displaySize}</p>
+    <p><span class="font-semibold">Chip Set:</span> ${phone.mainFeatures.chipSet}</p>
+    <p><span class="font-semibold">Memory:</span> ${phone.mainFeatures.memory}</p>
+    <p><span class="font-semibold">Features:</span> ${phone.mainFeatures.sensors}</p>
+    `
+    showPhoneDetails(data);
+}
+
+const showPhoneDetails = (phone) => {
+    my_modal_5.showModal();
+}
+
+loadPhone();
